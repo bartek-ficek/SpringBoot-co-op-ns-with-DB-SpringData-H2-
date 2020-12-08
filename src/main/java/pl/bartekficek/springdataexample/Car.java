4013b53c3ca2637df92860330386b3d0456ac4d3
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-08 10:34:41 +0100

    Entity Car created

---
+++ src/main/java/pl/bartekficek/springdataexample/Car.java
@@ -0,0 +1,69 @@
+package pl.bartekficek.springdataexample;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String carMake;
+    private String model;
+    private Color color;
+
+    public Car(String carMake, String model, Color color) {
+        this.carMake = carMake;
+        this.model = model;
+        this.color = color;
+    }
+
+    public Car() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCarMake() {
+        return carMake;
+    }
+
+    public void setCarMake(String carMake) {
+        this.carMake = carMake;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", carMake='" + carMake + '\'' +
+                ", model='" + model + '\'' +
+                ", color=" + color +
+                '}';
+    }
+}
---
+++ src/main/java/pl/bartekficek/springdataexample/Color.java
@@ -0,0 +1,5 @@
+package pl.bartekficek.springdataexample;
+
+public enum Color {
+    RED, BLUE, YELLOW, BLACK
+}
