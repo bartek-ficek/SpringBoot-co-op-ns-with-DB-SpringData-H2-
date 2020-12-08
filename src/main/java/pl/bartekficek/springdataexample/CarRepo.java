71fdab491a7bc2d5247fe435eb9a154ac34ebe9d
Bartek Ficek <ficek.bartlomiej@gmail.com>
2020-12-08 10:38:41 +0100

    Car Repository Interface created

---
+++ src/main/java/pl/bartekficek/springdataexample/CarRepo.java
@@ -0,0 +1,8 @@
+package pl.bartekficek.springdataexample;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepo extends CrudRepository<Car, Long> {
+}
